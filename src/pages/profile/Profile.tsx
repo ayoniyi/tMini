645da3397ccb6c352edce0613af4d79a32fcc509
ayoniyi <ayo.a.niyi@gmail.com>
2023-05-31 17:06:20 -0700

    style changes

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,149 +1,153 @@
-import { useContext, useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
-import { AuthContext } from '../../context/AuthContext'
-import { Link } from 'react-router-dom'
-import AppContainer from '../../components/AppContainer/AppContainer'
-import Nav from '../../components/Nav/Nav'
-import RightBar from '../../components/RightBar/RightBar'
-import PageContent from '../../components/PageContent/PageContent'
-import { CircularProgress } from '@material-ui/core'
-import { get, put } from '../../utils/axiosLib'
-import { logger } from '../../utils/logger'
-import { useParams } from 'react-router'
-import Update from '../../components/Update/Update'
-import Tweet from '../../components/Tweet/Tweet'
+import { useContext, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+import AppContainer from "../../components/AppContainer/AppContainer";
+import Nav from "../../components/Nav/Nav";
+import RightBar from "../../components/RightBar/RightBar";
+import PageContent from "../../components/PageContent/PageContent";
+import { CircularProgress } from "@material-ui/core";
+import { get, put } from "../../utils/axiosLib";
+import { logger } from "../../utils/logger";
+import { useParams } from "react-router";
+import Update from "../../components/Update/Update";
+import Tweet from "../../components/Tweet/Tweet";
 
-import style from './Profile.module.scss'
-import style2 from '../../components/Feed/Feed.module.scss'
-import arrow from '../../images/icons/arrow.svg'
-import location from '../../images/icons/locale.svg'
-import Cover from '../../images/others/cover.jpeg'
-import avi from '../../images/others/avatar.jpeg'
+import style from "./Profile.module.scss";
+import style2 from "../../components/Feed/Feed.module.scss";
+import arrow from "../../images/icons/arrow.svg";
+import location from "../../images/icons/locale.svg";
+import Cover from "../../images/others/cover.jpeg";
+import avi from "../../images/others/avatar.jpeg";
 
-const portalElement: any = document.getElementById('modalOverlay')
+const portalElement: any = document.getElementById("modalOverlay");
 
 interface Modal {
-  modal?: string
-  modalStatus?: boolean
-  modalState?: boolean
+  modal?: string;
+  modalStatus?: boolean;
+  modalState?: boolean;
 }
 interface Status {
-  errMsg?: string
-  isLoading?: boolean
-  isLoadingTweets?: boolean
+  errMsg?: string;
+  isLoading?: boolean;
+  isLoadingTweets?: boolean;
 }
 
 const Profile = () => {
-  const username = useParams().username
-  const [currentPage, setCurrentPage] = useState<string>('Profile')
-  const [paramUser, setParamUser] = useState<any>({})
+  const username = useParams().username;
+  const [currentPage, setCurrentPage] = useState<string>("Profile");
+  const [paramUser, setParamUser] = useState<any>({});
   const [status, setStatus] = useState<Status>({
-    errMsg: '',
+    errMsg: "",
     isLoading: true,
     isLoadingTweets: true,
-  })
-  const [authState, setAuthState] = useContext<any>(AuthContext)
-  const currentUser: any = authState.user.user
+  });
+  const [authState, setAuthState] = useContext<any>(AuthContext);
+  const currentUser: any = authState.user.user;
   const [modalValues, setModalValues] = useState<Modal>({
-    modal: 'None',
+    modal: "None",
     modalStatus: false,
     modalState: false,
-  })
+  });
   const [tweetView, setTweetView] = useState<any>({
-    tweetType: 'tweets',
+    tweetType: "tweets",
     currentTweets: [],
-  })
-  const [usersTweets, setUsersTweets] = useState<number>(0)
-  const [followed, setFollowed] = useState<boolean>()
+  });
+  const [usersTweets, setUsersTweets] = useState<number>(0);
+  const [followed, setFollowed] = useState<boolean>();
 
   useEffect(() => {
     const loadProfile = async () => {
       if (username === currentUser.username) {
-        setCurrentPage('Profile')
+        setCurrentPage("Profile");
       } else {
-        setCurrentPage('Home')
+        setCurrentPage("Home");
       }
       try {
-        const endpoint = `${process.env.REACT_APP_BASE_URL}user?username=${username}`
-        const profileReq = await get(endpoint)
+        const endpoint = `${process.env.REACT_APP_BASE_URL}user?username=${username}`;
+        const profileReq = await get(endpoint);
         //logger(profileReq.data)
-        setParamUser(profileReq.data)
+        setParamUser(profileReq.data);
         // setParamUser((prevState: any) => {
         //   return { ...prevState, paramUser: profileReq.data }
         // })
         setStatus({
-          errMsg: '',
+          errMsg: "",
           isLoading: false,
-        })
+        });
+        logger("profile>>>", profileReq.data);
       } catch (err) {
         //}catch(err: any | undefined | {}){
-        // logger(err)
+
+        //paramUser.profilePicture
         setStatus({
-          errMsg: 'Sorry an error occured...',
+          errMsg: "Sorry an error occured...",
           isLoading: false,
-        })
+        });
       }
-    }
-    loadProfile()
-  }, [username, currentUser.username, authState.latestFollow])
+    };
+    loadProfile();
+  }, [username, currentUser.username, authState.latestFollow]);
 
   useEffect(() => {
     const loadTweets = async () => {
       try {
-        const endpoint = `${process.env.REACT_APP_BASE_URL}post/tweets/${username}`
-        const tweetsReq = await get(endpoint)
+        const endpoint = `${process.env.REACT_APP_BASE_URL}post/tweets/${username}`;
+        const tweetsReq = await get(endpoint);
         //logger('tweets', tweetsReq.data)
         setTweetView({
-          tweetType: 'tweets',
+          tweetType: "tweets",
           currentTweets: tweetsReq.data,
-        })
-        setUsersTweets(tweetsReq.data.length)
+        });
+        setUsersTweets(tweetsReq.data.length);
         setStatus({
-          errMsg: '',
+          errMsg: "",
           isLoading: false,
           isLoadingTweets: false,
-        })
+        });
       } catch (err) {
         //}catch(err: any | undefined | {}){
         // logger(err)
         setStatus({
-          errMsg: 'Tweets are not loading right now...',
+          errMsg: "Tweets are not loading right now...",
           isLoading: false,
           isLoadingTweets: false,
-        })
+        });
       }
-    }
-    loadTweets()
-  }, [username])
+    };
+    loadTweets();
+  }, [username]);
 
   const handleModal = () => {
     setTimeout(() => {
       setModalValues({
         ...modalValues,
-        modal: 'None',
+        modal: "None",
         modalState: false,
-      })
-    }, 300)
+      });
+    }, 300);
     setModalValues({
       ...modalValues,
       modalStatus: false,
-    })
-  }
+    });
+  };
   const handleUpdate = () => {
     setModalValues({
       ...modalValues,
-      modal: 'Update',
+      modal: "Update",
       modalState: true,
       modalStatus: true,
-    })
-  }
+    });
+  };
   const handleFollow = async () => {
     try {
       if (followed) {
-        setFollowed(false)
+        setFollowed(false);
         const endpoint =
-          `${process.env.REACT_APP_BASE_URL}user/` + paramUser._id + '/unfollow'
-        const unfollowReq = await put(endpoint, { userId: currentUser._id })
+          `${process.env.REACT_APP_BASE_URL}user/` +
+          paramUser._id +
+          "/unfollow";
+        const unfollowReq = await put(endpoint, { userId: currentUser._id });
         setAuthState({
           ...authState,
           user: {
@@ -151,19 +155,19 @@ const Profile = () => {
             user: {
               ...authState.user.user,
               following: authState.user.user.following.filter(
-                (following: any) => following !== paramUser._id,
+                (following: any) => following !== paramUser._id
               ),
             },
           },
           latestFollow: Math.random() * 10,
-        })
-        logger('unfollow>>', unfollowReq)
-        logger(authState.user.user.following)
+        });
+        logger("unfollow>>", unfollowReq);
+        logger(authState.user.user.following);
       } else {
-        setFollowed(true)
+        setFollowed(true);
         const endpoint =
-          `${process.env.REACT_APP_BASE_URL}user/` + paramUser._id + '/follow'
-        const followReq = await put(endpoint, { userId: currentUser._id })
+          `${process.env.REACT_APP_BASE_URL}user/` + paramUser._id + "/follow";
+        const followReq = await put(endpoint, { userId: currentUser._id });
         setAuthState({
           ...authState,
           user: {
@@ -174,137 +178,141 @@ const Profile = () => {
             },
           },
           latestFollow: Math.random() * 10,
-        })
-        logger('follow>>', followReq)
-        logger(authState.user.user.following)
+        });
+        logger("follow>>", followReq);
+        logger(authState.user.user.following);
       }
     } catch (err) {
-      logger(err)
+      logger(err);
     }
-  }
+  };
   useEffect(() => {
-    setFollowed(currentUser.following.includes(paramUser._id))
-    console.log('do i follow?  ', currentUser.following.includes(paramUser._id))
+    setFollowed(currentUser.following.includes(paramUser._id));
+    console.log(
+      "do i follow?  ",
+      currentUser.following.includes(paramUser._id)
+    );
     //logger('url user??  ', paramUser._id)
-    console.log('current context >>> ', currentUser.following)
-  }, [currentUser.following, paramUser._id])
+    //console.log('current context >>> ', currentUser.following)
+    console.log("img>>>");
+  }, [currentUser.following, paramUser._id]);
 
   const loadTweets = async () => {
     setStatus({
-      errMsg: '',
+      errMsg: "",
       isLoading: false,
       isLoadingTweets: true,
-    })
+    });
     try {
-      const endpoint = `${process.env.REACT_APP_BASE_URL}post/tweets/${username}`
-      const tweetsReq = await get(endpoint)
-      logger('tweets', tweetsReq.data)
+      const endpoint = `${process.env.REACT_APP_BASE_URL}post/tweets/${username}`;
+      const tweetsReq = await get(endpoint);
+      logger("tweets", tweetsReq.data);
       setTweetView({
-        tweetType: 'tweets',
+        tweetType: "tweets",
         currentTweets: tweetsReq.data,
-      })
+      });
       setStatus({
-        errMsg: '',
+        errMsg: "",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     } catch (err) {
       //}catch(err: any | undefined | {}){
       // logger(err)
       setStatus({
-        errMsg: 'Tweets are not loading right now...',
+        errMsg: "Tweets are not loading right now...",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     }
-  }
+  };
   const loadRetweets = async () => {
     setStatus({
-      errMsg: '',
+      errMsg: "",
       isLoading: false,
       isLoadingTweets: true,
-    })
+    });
     try {
-      const endpoint = `${process.env.REACT_APP_BASE_URL}post/retweets/${username}`
-      const tweetsReq = await get(endpoint)
-      logger('tweets', tweetsReq.data)
+      const endpoint = `${process.env.REACT_APP_BASE_URL}post/retweets/${username}`;
+      const tweetsReq = await get(endpoint);
+      logger("tweets", tweetsReq.data);
       setTweetView({
-        tweetType: 'retweets',
+        tweetType: "retweets",
         currentTweets: tweetsReq.data,
-      })
+      });
       setStatus({
-        errMsg: '',
+        errMsg: "",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     } catch (err) {
       //}catch(err: any | undefined | {}){
       // logger(err)
       setStatus({
-        errMsg: 'Tweets are not loading right now...',
+        errMsg: "Tweets are not loading right now...",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     }
-  }
+  };
   const loadLikes = async () => {
     setStatus({
-      errMsg: '',
+      errMsg: "",
       isLoading: false,
       isLoadingTweets: true,
-    })
+    });
     try {
-      const endpoint = `${process.env.REACT_APP_BASE_URL}post/likes/${username}`
-      const tweetsReq = await get(endpoint)
-      logger('likes', tweetsReq.data)
+      const endpoint = `${process.env.REACT_APP_BASE_URL}post/likes/${username}`;
+      const tweetsReq = await get(endpoint);
+      logger("likes", tweetsReq.data);
       setTweetView({
-        tweetType: 'likes',
+        tweetType: "likes",
         currentTweets: tweetsReq.data,
-      })
+      });
       setStatus({
-        errMsg: '',
+        errMsg: "",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     } catch (err) {
       //}catch(err: any | undefined | {}){
       // logger(err)
       setStatus({
-        errMsg: 'Tweets are not loading right now...',
+        errMsg: "Tweets are not loading right now...",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     }
-  }
+  };
   const loadMedia = async () => {
     setStatus({
-      errMsg: '',
+      errMsg: "",
       isLoading: false,
       isLoadingTweets: true,
-    })
+    });
     try {
-      const endpoint = `${process.env.REACT_APP_BASE_URL}post/media/${username}`
-      const tweetsReq = await get(endpoint)
-      logger('tweets', tweetsReq.data)
+      const endpoint = `${process.env.REACT_APP_BASE_URL}post/media/${username}`;
+      const tweetsReq = await get(endpoint);
+      logger("tweets", tweetsReq.data);
       setTweetView({
-        tweetType: 'media',
+        tweetType: "media",
         currentTweets: tweetsReq.data,
-      })
+      });
       setStatus({
-        errMsg: '',
+        errMsg: "",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     } catch (err) {
       //}catch(err: any | undefined | {}){
       // logger(err)
       setStatus({
-        errMsg: 'Tweets are not loading right now...',
+        errMsg: "Tweets are not loading right now...",
         isLoading: false,
         isLoadingTweets: false,
-      })
+      });
     }
-  }
+  };
   return (
     <AppContainer>
       <Nav currentPage={currentPage} paramUser={paramUser} />
@@ -319,13 +327,13 @@ const Profile = () => {
               {ReactDOM.createPortal(
                 <div
                   className={
-                    style.overlay + ' animate__animated animate__fadeIn'
+                    style.overlay + " animate__animated animate__fadeIn"
                   }
                   onClick={handleModal}
                 ></div>,
-                portalElement,
+                portalElement
               )}
-              {modalValues.modal === 'Update' && (
+              {modalValues.modal === "Update" && (
                 <Update
                   handleModal={handleModal}
                   modalState={modalValues.modalState}
@@ -359,8 +367,8 @@ const Profile = () => {
                 )}
                 {paramUser.username !== currentUser.username && (
                   <button onClick={handleFollow}>
-                    {' '}
-                    {followed ? 'Unfollow' : 'Follow'}
+                    {" "}
+                    {followed ? "Unfollow" : "Follow"}
                   </button>
                 )}
               </div>
@@ -388,25 +396,25 @@ const Profile = () => {
               <div className={style.typesTop}>
                 <div className={style.typeSingle} onClick={loadTweets}>
                   <h2>Tweets</h2>
-                  {tweetView.tweetType === 'tweets' && (
+                  {tweetView.tweetType === "tweets" && (
                     <div className={style.line}></div>
                   )}
                 </div>
                 <div className={style.typeSingle} onClick={loadRetweets}>
                   <h2>Retweets</h2>
-                  {tweetView.tweetType === 'retweets' && (
+                  {tweetView.tweetType === "retweets" && (
                     <div className={style.line}></div>
                   )}
                 </div>
                 <div className={style.typeSingle} onClick={loadMedia}>
                   <h2>Media</h2>
-                  {tweetView.tweetType === 'media' && (
+                  {tweetView.tweetType === "media" && (
                     <div className={style.line}></div>
                   )}
                 </div>
                 <div className={style.typeSingle} onClick={loadLikes}>
                   <h2>Likes</h2>
-                  {tweetView.tweetType === 'likes' && (
+                  {tweetView.tweetType === "likes" && (
                     <div className={style.line}></div>
                   )}
                 </div>
@@ -424,11 +432,11 @@ const Profile = () => {
                       ))}
                     {tweetView.currentTweets.length < 1 && (
                       <div className={style2.loaderBox}>
-                        <p style={{ color: '#fff' }}>Nothing to see here yet</p>
+                        <p style={{ color: "#fff" }}>Nothing to see here yet</p>
                       </div>
                     )}
                     <div className={style2.loaderBox}>
-                      <p style={{ color: '#fff' }}>{status.errMsg}</p>
+                      <p style={{ color: "#fff" }}>{status.errMsg}</p>
                     </div>
                   </>
                 )}
@@ -439,7 +447,7 @@ const Profile = () => {
       )}
       <RightBar />
     </AppContainer>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
